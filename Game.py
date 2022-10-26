72a9987682c5ba59cdfeefa4fbc1b76f74a938b2
NicknamesAreNotHard <caesarflyons@hotmail.com>
2022-10-26 18:30:21 +0200

    Pieces
    Added the pieces names (im working hard)

--- Game.py
+++ Game.py
@@ -13,3 +13,18 @@ class Piece:
         self.type = type
         self.killable = killable
         self.image = image
+
+bPawn = Piece('b', 'p', 'b_pawn.png')
+wPawn = Piece('w', 'p', 'w_pawn.png')
+bKing = Piece('b', 'k', 'b_king.png')
+wKing = Piece('w', 'k', 'w_king.png')
+bRook = Piece('b', 'r', 'b_rook.png')
+wRook = Piece('w', 'r', 'w_rook.png')
+bBishop = Piece('b', 'b', 'b_bishop.png')
+wBishop = Piece('w', 'b', 'w_bishop.png')
+bQueen = Piece('b', 'q', 'b_queen.png')
+wQueen = Piece('w', 'q', 'w_queen.png')
+bKnight = Piece('b', 'kn', 'b_knight.png')
+wKnight = Piece('w', 'kn', 'w_knight.png')
+wKnook = Piece('w', 'kno', 'w_knook.png')
+bKnook = Piece('w', 'kno', 'w_knook.png')
