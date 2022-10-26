33cf948e306622c2e9bba93691ddb8dcc264e46c
NicknamesAreNotHard <caesarflyons@hotmail.com>
2022-10-26 18:01:01 +0200

    Chess Pieces 2.0

--- Game.py
+++ Game.py
@@ -1,8 +1,15 @@
 import pygame
-
 import time
-
 import sys
 
 board = [['  ' for i in range(8)] for i in range(8)]
-print(board)
+
+
+## Creates a chess piece class that shows what team a piece is on,
+# what type of piece it is and whether or not it can be killed by another selected piece.
+class Piece:
+    def __init__(self, team, type, image, killable=False):
+        self.team = team
+        self.type = type
+        self.killable = killable
+        self.image = image
