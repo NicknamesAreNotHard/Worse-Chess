ae1a0a954db012ccc7c35f40cf68444474f26e79
NicknamesAreNotHard <caesarflyons@hotmail.com>
2022-11-09 17:59:31 +0100

    bemis

--- Chess/Game.py
+++ Chess/Game.py
@@ -36,4 +36,22 @@ starting_order = {(0, 0): pygame.image.load(bRook.image), (1, 0): pygame.image.l
                   (0, 1): pygame.image.load(bPawn.image), (1, 1): pygame.image.load(bPawn.image),
                   (2, 1): pygame.image.load(bPawn.image), (3, 1): pygame.image.load(bPawn.image),
                   (4, 1): pygame.image.load(bPawn.image), (5, 1): pygame.image.load(bPawn.image),
-                  (6, 1): pygame.image.load(bPawn.image), (7, 1): pygame.image.load(bPawn.image),}
+                  (6, 1): pygame.image.load(bPawn.image), (7, 1): pygame.image.load(bPawn.image),
+
+                  (0, 2): None, (1, 2): None, (2, 2): None, (3, 2): None,
+                  (4, 2): None, (5, 2): None, (6, 2): None, (7, 2): None,
+                  (0, 3): None, (1, 3): None, (2, 3): None, (3, 3): None,
+                  (4, 3): None, (5, 3): None, (6, 3): None, (7, 3): None,
+                  (0, 4): None, (1, 4): None, (2, 4): None, (3, 4): None,
+                  (4, 4): None, (5, 4): None, (6, 4): None, (7, 4): None,
+                  (0, 5): None, (1, 5): None, (2, 5): None, (3, 5): None,
+                  (4, 5): None, (5, 5): None, (6, 5): None, (7, 5): None,
+
+                  (0, 6): pygame.image.load(wPawn.image), (1, 6): pygame.image.load(wPawn.image),
+                  (2, 6): pygame.image.load(wPawn.image), (3, 6): pygame.image.load(wPawn.image),
+                  (4, 6): pygame.image.load(wPawn.image), (5, 6): pygame.image.load(wPawn.image),
+                  (6, 6): pygame.image.load(wPawn.image), (7, 6): pygame.image.load(wPawn.image),
+                  (0, 7): pygame.image.load(wRook.image), (1, 7): pygame.image.load(wKnight.image),
+                  (2, 7): pygame.image.load(wBishop.image), (3, 7): pygame.image.load(wKing.image),
+                  (4, 7): pygame.image.load(wQueen.image), (5, 7): pygame.image.load(wBishop.image),
+                  (6, 7): pygame.image.load(wKnight.image), (7, 7): pygame.image.load(wRook.image),}
